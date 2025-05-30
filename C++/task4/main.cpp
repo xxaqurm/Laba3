3d36034b27100639a9bba34eb9c7fd4b07c6d3a9
xxaqurm <tohanesterov003@gmail.com>
2025-05-30 07:35:00 +0700

    Доработана 4 задача

--- C++/task4/main.cpp
+++ C++/task4/main.cpp
@@ -12,21 +12,25 @@ int main() {
     vector<int> a(n);
     for (int &x : a) cin >> x;
 
+    vector<int> prefix(n + 1, 0);
+    for (int i = 0; i < n; ++i) {
+        prefix[i + 1] = prefix[i] + a[i];
+    }
+
     vector<int> dp(n + 1, 0);
 
-    for (int i = n - 1; i >= 0; --i) {
-        int sum = 0;
-        int best = -1000;
+    for (int i = n - 1; i >= 0; i--) {
+        int best = -1000000000;
         for (int k = 1; k <= m && i + k <= n; k++) {
-            sum += a[i + k - 1];
+            int sum = prefix[i + k] - prefix[i];
             best = max(best, sum - dp[i + k]);
         }
         dp[i] = best;
     }
 
     if (dp[0] > 0) {
-        cout << "Победила Вика!" << endl;
-    } else {
         cout << "Победил Паша!" << endl;
+    } else {
+        cout << "Победила Вика!" << endl;
     }
 }
