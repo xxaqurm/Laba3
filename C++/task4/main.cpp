69cfd8ec6f85ed6eebd9d6bd15b34d74d30b5033
xxaqurm <tohanesterov003@gmail.com>
2025-05-30 07:23:45 +0700

    Исправлено 4 задание

--- C++/task4/main.cpp
+++ C++/task4/main.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <climits>
-
 using namespace std;
 
 int main() {
     int n, m;
-    cout << "Введите свои n и m: ";
+    cout << "Введите П и В: ";
     cin >> n >> m;
 
+    cout << "Введите " << n << " целых чисел: ";
     vector<int> a(n);
-    vector<int> prefix_sum(n + 1, 0);  // префиксные суммы
-    for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-        prefix_sum[i + 1] = prefix_sum[i] + a[i];
-    }
+    for (int &x : a) cin >> x;
 
-    vector<int> dp(n + 1, INT_MIN);  // dp[i] - макс разница очков от позиции i
-    dp[n] = 0; // если всё удалено, разность 0
+    vector<int> dp(n + 1, 0);
 
-    // Обрабатываем позиции с конца к началу
-    for (int i = n - 1; i >= 0; i--) {
-        for (int j = 1; j <= m && i + j <= n; j++) {
-            int sum = prefix_sum[i + j] - prefix_sum[i]; // сумма взятых чисел
-            dp[i] = max(dp[i], sum - dp[i + j]); // макс разность очков
+    for (int i = n - 1; i >= 0; --i) {
+        int sum = 0;
+        int best = -1000;
+        for (int k = 1; k <= m && i + k <= n; k++) {
+            sum += a[i + k - 1];
+            best = max(best, sum - dp[i + k]);
         }
+        dp[i] = best;
     }
 
-    cout << (dp[0] > 0 ? 1 : 0) << endl;
+    if (dp[0] > 0) {
+        cout << "Победила Вика!" << endl;
+    } else {
+        cout << "Победил Паша!" << endl;
+    }
 }
